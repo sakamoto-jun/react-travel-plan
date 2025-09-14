ecf92af6a644c4d9756881ea539ed6cb3da8e222
sakamoto-jun <sakamotojun.dev@gmail.com>
2025-09-15 02:56:49 +0900

    chore: tailwindcss 설정 추가

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "./client/tailwind/colors";
+
+const px0_200 = Object.fromEntries(
+  Array.from({ length: 201 }, (_, i) => [i, `${i}px`])
+);
+const px0_20 = Object.fromEntries(
+  Array.from({ length: 21 }, (_, i) => [i, `${i}px`])
+);
+
+export default {
+  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: px0_200,
+      borderRadius: px0_20,
+      borderWidth: px0_20,
+      fontSize: px0_200,
+    },
+    colors,
+  },
+  plugins: [],
+};
